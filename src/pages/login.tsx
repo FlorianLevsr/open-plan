9409d602614ef2298780386386ff224b6a559836
Romain Tello <romain.tello@gmail.com>
2021-06-29 13:25:59 +0200

    Refactor login form as own component

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,46 +1,20 @@
-import React, { ChangeEvent, FormEvent, useState } from 'react'
+import React from 'react'
 import { useRouter } from 'next/router'
 import { GetServerSideProps, NextPage } from 'next'
-import {
-  FormControl,
-  FormLabel,
-  Button,
-  Input,
-  Center,
-  Alert,
-  AlertDescription,
-  AlertIcon,
-  AlertTitle,
-} from '@chakra-ui/react'
 import { Layout } from '../common/components/layouts'
 import { useAuthContext } from '../common/data/auth'
 import { getServerSidePropsWithAuthentication } from '../common/utils'
-
-type LoginFormInputType = 'username' | 'password'
-
-type LoginFormData = Record<LoginFormInputType, string>
+import { LoginForm } from '../common/components'
 
 const LoginPage: NextPage = () => {
   const router = useRouter()
   const { actions } = useAuthContext()
-  const [login, { error, loading }] = actions.useLogin()
-  const [formData, setFormData] = useState<LoginFormData>({
-    username: '',
-    password: '',
-  })
-  // const [error, setError] = useState<Error | undefined>()
+  const [login, mutationResult] = actions.useLogin()
 
-  const onChangeHandler = (
-    inputName: LoginFormInputType,
-    inputValue: string
-  ): void => {
-    const newFormData = { ...formData, [inputName]: inputValue }
-    setFormData(newFormData)
-  }
-
-  const onSubmitHandler = async (event: FormEvent): Promise<void> => {
-    event.preventDefault()
-    const { username, password } = formData
+  const callback = async (
+    username: string,
+    password: string
+  ): Promise<void> => {
     try {
       await login({ variables: { username, password } })
       router.push('/tasks')
@@ -50,48 +24,7 @@ const LoginPage: NextPage = () => {
 
   return (
     <Layout>
-      {error && (
-        <Alert status="error">
-          <AlertIcon />
-          <AlertTitle mr={2}>Error:</AlertTitle>
-          <AlertDescription>{error.message}</AlertDescription>
-        </Alert>
-      )}
-      <form>
-        <FormControl>
-          <FormLabel>
-            Username :
-            <Input
-              type="text"
-              name="username"
-              onChange={(event: ChangeEvent<HTMLInputElement>) =>
-                onChangeHandler('username', event.target.value)
-              }
-            />
-          </FormLabel>
-          <FormLabel>
-            Password :
-            <Input
-              type="text"
-              name="password"
-              onChange={(event: ChangeEvent<HTMLInputElement>) =>
-                onChangeHandler('password', event.target.value)
-              }
-            />
-          </FormLabel>
-          <Center>
-            <Button
-              isLoading={loading}
-              type="submit"
-              bg="teal"
-              color="white"
-              onClick={(event) => onSubmitHandler(event)}
-            >
-              Log in
-            </Button>
-          </Center>
-        </FormControl>
-      </form>
+      <LoginForm callback={callback} mutationResult={mutationResult} />
     </Layout>
   )
 }
